3a1d09a0af0bd811d5cedaf475867a25e7888042
Ledroff <ledroff@gmail.com>
2025-06-28 21:59:09 +0200

    Add temporary ReservationForm to fix Vercel build

---
+++ src/pages/ReservationForm.js
@@ -0,0 +1,12 @@
+// 🚩 src/pages/ReservationForm.js - Formulaire temporaire Crêperie de Saint Côme
+
+import React from 'react';
+
+export default function ReservationForm() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Formulaire de réservation</h1>
+      <p>Le formulaire de réservation sera bientôt disponible.</p>
+    </div>
+  );
+}
